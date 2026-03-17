7df700904fe92a184e0d8e09090c74f8460fcf81
geckoFooBar <mudholkarrohan23@gmail.com>
2026-03-17 14:49:21 +0530

    Remove comments

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-// Import any global components here, like your GenerateQr component
+
 import { GenerateQr } from './features/components/generate-qr/generate-qr';
 import { Description } from './features/components/description/description';
 import { Header } from './features/components/header/header';
@@ -8,7 +7,7 @@ import { Header } from './features/components/header/header';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [GenerateQr, Description, Header], // Add imports directly to the component
+  imports: [GenerateQr, Description, Header],
   template: `
   <div class="flex flex-col min-h-screen">
     <app-header></app-header>
