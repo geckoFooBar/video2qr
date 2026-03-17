2f3e0230de76800ea451ceb3840d045f683cd9a2
geckoFooBar <mudholkarrohan23@gmail.com>
2026-03-17 11:02:50 +0530

    Add functional buttons and description

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,22 +2,22 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 // Import any global components here, like your GenerateQr component
 import { GenerateQr } from './features/components/generate-qr/generate-qr';
+import { Description } from './features/components/description/description';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [GenerateQr], // Add imports directly to the component
+  imports: [GenerateQr, Description], // Add imports directly to the component
   template: `
-    <h1>{{ title }}</h1>
-    <app-generate-qr></app-generate-qr>
+  <div class="flex flex-col min-h-screen">
+    <h1 class="text-3xl font-bold text-left my-6 mx-6 w-max">{{ title }}</h1>
+    <div class="flex-grow">
+      <app-description></app-description>
+      <app-generate-qr></app-generate-qr>
+    </div>
+  </div>
   `,
-  styles: `
-    h1 {
-      font-family: Helvetica;
-      font-size: 24px;
-    }
-    `
 })
 export class App {
-  title: any = 'Video2QR';
+  title: string = 'video2QR';
 }
